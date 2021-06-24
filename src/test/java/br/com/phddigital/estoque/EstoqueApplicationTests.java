4038476340d744023edf7252353aaf4a57cc824c
Phelipp Nascimento <phelippnd.programador@gmail.com>
2021-06-23 22:53:47 -0300

    Begin Project

---
+++ src/test/java/br/com/phddigital/estoque/EstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phddigital.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
